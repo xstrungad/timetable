cb5995d3d7cc4c87c196b28a0ec4828dcdd70dd6
xstrungad <xstrungad@stuba.sk>
2024-01-14 06:34:17 +0100

    ain

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -8,9 +8,9 @@ use App\Models\Teacher;
 class TeacherController extends Controller
 {
     public function index() {
-        $teachers = Teacher::all(); // vygeneruje všetky údaje z tabuľky teachers 
-        //$teachers = Teacher::orderBy('lastname', 'ASC')->get(); // vygeneruje všetky údaje z tabuľky teachers a zoradí ich podľa priezviska
-        //$teachers = Teacher::orderBy('lastname', 'ASC')->orderBy('firstname', 'ASC')->get(); // vygeneruje všetky údaje z tabuľky teachers a zoradí ich podľa priezviska a mena      
+        $teachers = Teacher::all();
+        //$teachers = Teacher::orderBy('lastname', 'ASC')->get(); 
+        //$teachers = Teacher::orderBy('lastname', 'ASC')->orderBy('firstname', 'ASC')->get(); 
         return view('teachers.index')->with('teachers', $teachers);
     }
 }
