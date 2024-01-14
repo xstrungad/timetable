8a7709ffcdca7e2dda4c63b4f3d5594001e78a19
xstrungad <xstrungad@stuba.sk>
2024-01-14 06:38:15 +0100

    ain

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -7,10 +7,62 @@ use App\Models\Teacher;
 
 class TeacherController extends Controller
 {
-    public function index() {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
         $teachers = Teacher::all();
         //$teachers = Teacher::orderBy('lastname', 'ASC')->get(); 
         //$teachers = Teacher::orderBy('lastname', 'ASC')->orderBy('firstname', 'ASC')->get(); 
         return view('teachers.index')->with('teachers', $teachers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
